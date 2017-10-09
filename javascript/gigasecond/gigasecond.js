a02f5efe5c5578dd3e2639a83b2452fa05fc72e9
Phil Salant <PSalant@gmail.com>
2017-10-09 12:29:34 -0400

    First attempt to solve gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,3 +1,70 @@
-class Gigasecond {}
+class Gigasecond {
+  constructor(date) {
+    this.startDate = date;
+    this.endDate = this.dateAdd(this.startDate, 'second', 1000000000);
+  }
+
+  date() {
+    return this.endDate;
+  }
+
+  /**
+   * @param date  Date to start with
+   * @param interval  One of: year, quarter, month, week, day, hour, minute, second
+   * @param units  Number of units of the given interval to add.
+   */
+  dateAdd(date, interval, units) {
+    var ret = new Date(date);
+    
+    switch (interval.toLowerCase()) {
+      case 'year':
+        ret.setFullYear(ret.getFullYear() + units);
+        this.checkRollover();
+        break;
+
+      case 'quarter':
+        ret.setMonth(ret.getMonth() + 3 * units);
+        this.checkRollover();
+        break;
+
+      case 'month':
+        ret.setMonth(ret.getMonth() + units);
+        this.checkRollover();
+        break;
+
+      case 'week':
+        ret.setDate(ret.getDate() + 7 * units);
+        break;
+
+      case 'day':
+        ret.setDate(ret.getDate() + units);
+        break;
+
+      case 'hour':
+        ret.setTime(ret.getTime() + units * 3600000);
+        break;
+
+      case 'minute':
+        ret.setTime(ret.getTime() + units * 60000);
+        break;
+
+      case 'second':
+        ret.setTime(ret.getTime() + units * 1000);
+        break;
+
+      default:
+        ret = undefined;
+        break;
+    }
+
+    return ret;
+  }
+
+  checkRollover() {
+    if (ret.getDate() != date.getDate()) {
+      ret.setDate(0);
+    };
+  }
+}
 
 module.exports = Gigasecond;
