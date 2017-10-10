fde103349e2a6b84945ab2786011647b7bf718f3
Phil Salant <PSalant@gmail.com>
2017-10-10 00:05:10 -0400

    Optimize space-age

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,57 +1,35 @@
 const EARTH_YEAR = 31557600;
-const YEAR_LENGTHS = {
-  'mercury' : 0.2408467,
-  'venus' : 0.61519726,
-  'earth' : 0.2408467,
-  'mars' : 1.8808158,
-  'jupiter' : 11.862615,
-  'saturn' : 29.447498,
-  'uranus' : 84.016846,
-  'neptune' : 164.79132
+const YEAR_MULTIPLIERS = {
+  'Mercury' : 0.2408467,
+  'Venus' : 0.61519726,
+  'Earth' : 1,
+  'Mars' : 1.8808158,
+  'Jupiter' : 11.862615,
+  'Saturn' : 29.447498,
+  'Uranus' : 84.016846,
+  'Neptune' : 164.79132
 };
 
 class SpaceAge {
   constructor(seconds) {
     this.seconds = seconds;
+    this.generateConversions();
   }
 
-  onMercury() {
-    return this.getAge('mercury');
-  }
+  generateConversions() {
+    for (let planet in YEAR_MULTIPLIERS) {
+      if (YEAR_MULTIPLIERS.hasOwnProperty(planet)) {
 
-  onVenus() {
-    return this.getAge('venus');
-  }
+        this['on' + planet] = function() {
+          let yearLength = YEAR_MULTIPLIERS[planet] * EARTH_YEAR;
+          let age = this.seconds / yearLength;
 
-  onEarth() {
-    return Math.round(this.seconds / EARTH_YEAR * 100) / 100;
+          return Math.round(age * 100) / 100;
+        };
+        
+      }
+    }
   }
-
-  onMars() {
-    return this.getAge('mars');
-  }
-
-  onJupiter() {
-    return this.getAge('jupiter');
-  }
-
-  onSaturn() {
-    return this.getAge('saturn');
-  }
-
-  onUranus() {
-    return this.getAge('uranus');
-  }
-
-  onNeptune() {
-    return this.getAge('neptune');
-  }
-
-  getAge(planet) {
-    let yearLength = YEAR_LENGTHS[planet] * EARTH_YEAR;
-    let age = this.seconds / yearLength;
-    return Math.round(age * 100) / 100;
-  }
-};
+}
 
 module.exports = SpaceAge;
